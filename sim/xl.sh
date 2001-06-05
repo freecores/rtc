ecf9a93b22070739812ccd6631abf9ee2ded7e32
lampret <lampret@76562cd9-5698-43e4-9f5b-89ef699ce21a>
2001-06-05 08:07:51 +0000

    Typo.

--- sim/xl.sh
+++ sim/xl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd ../sim
 verilog +turbo+3 -q ../bench/wb_master.v ../bench/clkrst.v ../bench/tb_top.v ../bench/tb_tasks.v \
-../rtl/rtc_sim0.v +incdir+../rtl/ +incdir+../bench/
+../rtl/rtc.v +incdir+../rtl/ +incdir+../bench/
